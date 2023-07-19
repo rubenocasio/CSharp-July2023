96e92997b4e2bf947fbfe6056bb278d8f9cf693c
Ruben Ocasio <rocasio@codingdojo.com>
2023-07-19 09:05:57 -0400

    Update MinHeap.js

--- Algos/MinHeap/MinHeap.js
+++ Algos/MinHeap/MinHeap.js
@@ -79,27 +79,100 @@ class MinHeap {
    * - Space: O(1) constant.
    * @returns {?number} The min number or null if empty.
    */
-  extract() { }
-
   /**
-   * Logs the tree horizontally with the root on the left and the index in
-   * parenthesis using reverse inorder traversal.
-   */
-  printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
-    if (parentIdx > this.heap.length - 1) {
-      return;
+     * Extracts the min num from the heap and then re-orders the heap to
+     * maintain order so the next min is ready to be extracted.
+     * 1. Save the first node to a temp var.
+     * 2. Pop last node off and set idx1 equal to the popped value.
+     * 3. Iteratively swap the old last node that is now at idx1 with its
+     *    smallest child IF the smallest child is smaller than it.
+     * - Time: O(log n) logarithmic due to shiftDown.
+     * - Space: O(1) constant.
+     * @returns {?number} The min number or null if empty.
+     */
+  extract() {
+    if (this.heap.length <= 1) {
+      // If the heap is empty (only has the null placeholder), return null.
+      return null;
     }
 
-    spaceCnt += spaceIncr;
-    this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+    // Save the minimum value (root node) to a variable.
+    const min = this.heap[1];
 
-    console.log(
-      " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
-      `${this.heap[parentIdx]} (${parentIdx})`
-    );
+    // Get the index of the last element in the heap.
+    const lastIdx = this.heap.length - 1;
 
-    this.printHorizontalTree(parentIdx * 2, spaceCnt);
+    // Replace the root node with the last element and remove the last element.
+    this.heap[1] = this.heap[lastIdx];
+    this.heap.pop();
+
+    // Start from the root node and iterate to find the correct position for the new root.
+    let currentIndex = 1;
+
+    while (true) {
+      // Calculate the indices of the left and right child nodes.
+      const leftChildIndex = currentIndex * 2;
+      const rightChildIndex = currentIndex * 2 + 1;
+
+      // Variable to track the index of the smallest child node.
+      let smallestChildIndex = null;
+
+      // Check if the left child exists.
+      if (leftChildIndex < this.heap.length) {
+        smallestChildIndex = leftChildIndex;
+
+        // Check if the right child exists and is smaller than the left child.
+        if (
+          rightChildIndex < this.heap.length &&
+          this.heap[rightChildIndex] < this.heap[leftChildIndex]
+        ) {
+          smallestChildIndex = rightChildIndex;
+        }
+      }
+
+      // If there is a smallest child and it is smaller than the current node, swap them.
+      if (
+        smallestChildIndex !== null &&
+        this.heap[smallestChildIndex] < this.heap[currentIndex]
+      ) {
+        [this.heap[currentIndex], this.heap[smallestChildIndex]] = [
+          this.heap[smallestChildIndex],
+          this.heap[currentIndex],
+        ];
+
+        // Update the current index to continue the iteration with the smallest child.
+        currentIndex = smallestChildIndex;
+      } else {
+        // If the current node is in its correct position, break the loop.
+        break;
+      }
+    }
+
+    // Return the minimum value that was extracted from the heap.
+    return min;
   }
+
+
+
+/**
+ * Logs the tree horizontally with the root on the left and the index in
+ * parenthesis using reverse inorder traversal.
+ */
+printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
+  if (parentIdx > this.heap.length - 1) {
+    return;
+  }
+
+  spaceCnt += spaceIncr;
+  this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+
+  console.log(
+    " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+    `${this.heap[parentIdx]} (${parentIdx})`
+  );
+
+  this.printHorizontalTree(parentIdx * 2, spaceCnt);
+}
 }
 
 const testMinHeap = new MinHeap();
@@ -118,6 +191,27 @@ testMinHeap.insert(42);
 testMinHeap.insert(9);
 testMinHeap.insert(25);
 testMinHeap.insert(30);
+// console.log(testMinHeap.top())
 testMinHeap.printHorizontalTree()
 console.log("----------------------------------")
-console.log(testMinHeap.top())
+console.log(testMinHeap.extract())
+console.log("----------------------------------")
+testMinHeap.printHorizontalTree()
+console.log("----------------------------------")
+console.log(testMinHeap.extract())
+console.log("----------------------------------")
+testMinHeap.printHorizontalTree()
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log(testMinHeap.extract())
+console.log("----------------------------------")
+testMinHeap.printHorizontalTree()
