869bdb5137e93ca00f824215ced674cd9ccdd4b6
Ruben Ocasio <rocasio@codingdojo.com>
2023-07-21 09:23:29 -0400

    Update DoublyLinkedList.js

--- Algos/DLL/DoublyLinkedList.js
+++ Algos/DLL/DoublyLinkedList.js
@@ -46,7 +46,28 @@ class DoublyLinkedList {
    * @param {any} data The data for the new node.
    * @returns {DoublyLinkedList} This list.
    */
-  insertAtFront(data) { }
+  insertAtFront(data) {
+    // Create a new node with the given data.
+    const newHead = new DLLNode(data);
+
+    // Check if the list is empty.
+    if (this.isEmpty()) {
+      // If the list is empty, set both the head and tail to the new node.
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      // If the list is not empty:
+      // - Set the old head as the next node of the new node.
+      const oldHead = this.head;
+      oldHead.prev = newHead;
+      newHead.next = oldHead;
+      // - Update the head to be the new node.
+      this.head = newHead;
+    }
+
+    // Return the updated list.
+    return this;
+  }
 
   /**
    * Creates a new node and adds it at the back of this list.
@@ -55,7 +76,28 @@ class DoublyLinkedList {
    * @param {any} data The data for the new node.
    * @returns {DoublyLinkedList} This list.
    */
-  insertAtBack(data) { }
+  insertAtBack(data) {
+    // Create a new node with the given data.
+    const newTail = new DLLNode(data);
+
+    // Check if the list is empty.
+    if (this.isEmpty()) {
+      // If the list is empty, set both the head and tail to the new node.
+      this.head = newTail;
+      this.tail = newTail;
+    } else {
+      // If the list is not empty:
+      // - Set the next reference of the current tail to the new node.
+      this.tail.next = newTail;
+      // - Set the prev reference of the new node to the current tail.
+      newTail.prev = this.tail;
+      // - Update the tail to be the new node.
+      this.tail = newTail;
+    }
+
+    // Return the updated list.
+    return this;
+  }
 
   // EXTRA
   /**
@@ -64,8 +106,72 @@ class DoublyLinkedList {
    * - Space: O(?).
    * @returns {any} The data of the removed node.
    */
-  removeMiddleNode() { }
+  removeMiddleNode() {
+    // Check if there is only one node in the list.
+    if (!this.isEmpty() && this.head === this.tail) {
+      // If there is only one node, remove it by setting the head and tail to null.
+      const removedData = this.head.data;
+      this.head = null;
+      this.tail = null;
+      return removedData;
+    }
 
+    // Create two runners, one starting from the head and the other from the tail.
+    let forwardRunner = this.head;
+    let backwardsRunner = this.tail;
+
+    // Traverse the list towards the middle from both ends.
+    while (forwardRunner && backwardsRunner) {
+      // Check if the forward and backward runners have met at the same node (middle node).
+      if (forwardRunner === backwardsRunner) {
+        // Save the middle node reference.
+        const midNode = forwardRunner;
+
+        // Adjust the previous node's next reference to skip the middle node.
+        midNode.prev.next = midNode.next;
+        // Adjust the next node's previous reference to skip the middle node.
+        midNode.next.prev = midNode.prev;
+
+        // Return the data of the removed middle node.
+        return midNode.data;
+      }
+      // Check if the forward runner's previous node is the same as the backward runner,
+      // indicating that the runners have passed each other without stopping on the same node.
+      // This occurs in even-length lists and indicates that there is no middle node.
+      if (forwardRunner.prev === backwardsRunner) {
+        return null;
+      }
+      // Move the forward runner one step forward.
+      forwardRunner = forwardRunner.next;
+      // Move the backward runner one step backward.
+      backwardsRunner = backwardsRunner.prev;
+    }
+    // If no middle node is found, return null.
+    return null;
+  }
+
+  /**
+ * Inserts a new node with the given newVal after the node that has the
+ * given targetVal as it's data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} targetVal The node data to find.
+ * @param {any} newVal Data for the new node.
+ * @returns {boolean} Indicates if the new node was added.
+ */
+  insertAfter(targetVal, newVal) { }
+
+  /**
+   * Inserts a new node with the given newVal before the node that has the
+   * given targetVal as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} targetVal The node data to find.
+   * @param {any} newVal Data for the new node.
+   * @returns {boolean} Indicates if the new node was added.
+   */
+  insertBefore(targetVal, newVal) { }
+  
   /**
    * Determines if this list is empty.
    * - Time: O(1) constant.
