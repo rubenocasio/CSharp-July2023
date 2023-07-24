5bc497b697cff5b30e3a670b70e424024dc0b337
Ruben Ocasio <rocasio@codingdojo.com>
2023-07-24 11:17:20 -0400

    Stacks

---
+++ Algos/Stack/Stack.js
@@ -0,0 +1,137 @@
+/**
+ * Class to represent a stack using an array to store the stacked items.
+ * Follows a LIFO (Last In First Out) order where new items are stacked on
+ * top (back of array) and removed items are removed from the top / back.
+ */
+class Stack {
+    /**
+     * The constructor is executed when instantiating a new Stack() to construct
+     * a new instance.
+     * @returns {Stack} This new Stack instance is returned without having to
+     *    explicitly write 'return' (implicit return).
+     */
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Adds a new given item to the top / back of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to be added to the top / back.
+     * @returns {number} The new length of this stack.
+     */
+    push(item) { }
+
+    /**
+     * Removes the top / last item from this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item or undefined if this stack was empty.
+     */
+    pop() { }
+
+    /**
+     * Retrieves the top / last item from this stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The top / last item of this stack.
+     */
+    peek() { }
+
+    /**
+     * Returns whether or not this stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() { }
+
+    /**
+     * Returns the size of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() { }
+}
+
+let empty = new Stack();
+let myList = new Stack()
+
+class StackNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListStack {
+    constructor() {
+        this.head = null;
+    }
+
+    /**
+     * Adds a new item to the top of the stack (the head).
+     * - Time: O(1) constant.
+     * - Space: O(1).
+     * @param {any} val The val to add.
+     * @returns {void}
+     */
+    push(val) {}
+
+    /**
+     * Removes the top item (the head).
+     * - Time: O(1) constant.
+     * - Space: O(1).
+     * @returns {any} The top item of the stack.
+     */
+    pop() {}
+
+    /**
+     * Returns the top item of the stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1).
+     * @returns {any} The top item.
+     */
+    peek() {}
+
+    /**
+     * Determines if the stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1).
+     * @returns {boolean}
+     */
+    isEmpty() {}
+
+    /**
+     * Gets the count of items in the stack.
+     * - Time: O(n) linear, n = list length.
+     * - Space: O(1).
+     * @returns {number} The total number of items.
+     */
+    size() {}
+
+    /**
+     * Returns whether or not this stack contains the value passed in (val).
+     * @returns {val} value passed in.
+     */
+    contains(val) {}
+
+    // Time: O(n) linear, n = list length
+    // Space: O(n)
+    print() {
+        let runner = this.head;
+        let vals = "";
+
+        while (runner) {
+            vals += `${runner.data}${runner.next ? ", " : ""}`;
+            runner = runner.next;
+        }
+        console.log(vals);
+        return vals;
+    }
+}
+
+let emptyLLS = new LinkedListStack();
+let myListLLS = new LinkedListStack()
